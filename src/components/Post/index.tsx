4d2c25dff35310709188c06c495e0bd15700db43
Andrea Tupini <tupini07@gmail.com>
2020-04-12 10:46:05 -0600

    post component use single quotes

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { Link } from 'gatsby';
 import moment from 'moment';
+import React from 'react';
 import './style.scss';
 
 const Post = ({ data }) => {
@@ -8,25 +8,25 @@ const Post = ({ data }) => {
   const { slug, categorySlug } = data.node.fields;
 
   return (
-    <div className="post">
-      <div className="post__meta">
-        <time className="post__meta-time" dateTime={moment(date).format('MMMM D, YYYY')}>
+    <div className='post'>
+      <div className='post__meta'>
+        <time className='post__meta-time' dateTime={moment(date).format('MMMM D, YYYY')}>
           {moment(date).format('MMMM YYYY')}
         </time>
-        <span className="post__meta-divider" />
-        <span className="post__meta-category" key={categorySlug}>
-          <Link to={categorySlug} className="post__meta-category-link">
+        <span className='post__meta-divider' />
+        <span className='post__meta-category' key={categorySlug}>
+          <Link to={categorySlug} className='post__meta-category-link'>
             {category}
           </Link>
         </span>
       </div>
-      <h2 className="post__title">
-        <Link className="post__title-link" to={slug}>
+      <h2 className='post__title'>
+        <Link className='post__title-link' to={slug}>
           {title}
         </Link>
       </h2>
-      <p className="post__description">{description}</p>
-      <Link className="post__readmore" to={slug}>
+      <p className='post__description'>{description}</p>
+      <Link className='post__readmore' to={slug}>
         Read
       </Link>
     </div>
