35b585ee1f8185fb8cf7836f2a0254d8fc24a010
Andrea Tupini <tupini07@gmail.com>
2020-08-04 17:26:01 -0600

    convert script now includes wikipedia page in posts

--- scripts/import-org-roam/convert.py
+++ scripts/import-org-roam/convert.py
@@ -3,12 +3,14 @@ import os
 from pathlib import Path
 from datetime import datetime
 from typing import Union, List
+import requests
+import tqdm
 
 ZETTLDIR = "../../content/zettelkasten/"
 ORG_ROAM_DIR = "/home/andrea/Dropbox/org-roam/"
 
 
-def get_file_metadata(file_path: str, meta_name: str) -> str:
+def get_file_metadata(file_path: str, meta_name: str, raw=False) -> str:
     meta_real_name = f"#+{meta_name}: "
     with open(file_path, "r") as file:
         relevant_line = [x for x in file.readlines() if meta_name in x]
@@ -21,7 +23,10 @@ def get_file_metadata(file_path: str, meta_name: str) -> str:
         if " " in meta_value:
             meta_value = meta_value.split(" ")
 
-        return marshall_metadata_to_pandoc_variable(meta_name, meta_value)
+        if raw:
+            return meta_value
+        else:
+            return marshall_metadata_to_pandoc_variable(meta_name, meta_value)
 
 
 def marshall_metadata_to_pandoc_variable(var_name: str, metadata: Union[str, List[str]]) -> str:
@@ -37,7 +42,7 @@ if __name__ == "__main__":
     os.system(f'rm -rf {ZETTLDIR}')
     os.system(f'mkdir {ZETTLDIR}')
 
-    for ffile_path in [x for x in os.listdir(ORG_ROAM_DIR) if x.endswith(".org")]:
+    for ffile_path in tqdm.tqdm([x for x in os.listdir(ORG_ROAM_DIR) if x.endswith(".org")]):
         raw_fpath = ORG_ROAM_DIR + ffile_path
         f_path = Path(raw_fpath)
         f_stats = f_path.stat()
@@ -55,11 +60,27 @@ if __name__ == "__main__":
                              if x is not None
                              )
 
+        wikidata_entity = get_file_metadata(
+            raw_fpath, "wikidata_entity", raw=True)
+        if wikidata_entity and wikidata_entity != 'null':
+            # get the title of the respective wikipedia page
+            respective_wp_entity_response: str = requests.get(
+                f"https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&props=sitelinks&ids={wikidata_entity}&sitefilter=enwiki"
+            ).json()
+
+            try:
+                respective_wp_entity = respective_wp_entity_response[
+                    "entities"][wikidata_entity]["sitelinks"]["enwiki"]["title"]
+
+                variables += f' --variable wikipedia_link="https://en.wikipedia.org/wiki/{respective_wp_entity}"'
+            except KeyError:
+                print(f"Didn't find a corresponding wikipedia entry for entity '{wikidata_entity}'. File: {f_name}")
+
         os.system('pandoc --from org --to markdown '
-              '--template markdown.template '
-              f'--variable wid="WID:{f_name}" '
-              f'--variable last_modified_date="{mod_time.isoformat()}" '
-              f'{variables} '
-              f'{f_path.absolute()} | '
-              f'sed -re "s/\]\(20/\]\(WID:20/" | sed -re "s/\.org\)/\)/g" '
-              f'> {output_path}')
+                  '--template markdown.template '
+                  f'--variable wid="WID:{f_name}" '
+                  f'--variable last_modified_date="{mod_time.isoformat()}" '
+                  f'{variables} '
+                  f'{f_path.absolute()} | '
+                  f'sed -re "s/\]\(20/\]\(WID:20/" | sed -re "s/\.org\)/\)/g" '
+                  f'> {output_path}')
